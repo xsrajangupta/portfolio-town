f203a237c4152e92071c929145c786ba885ecdd5
Srajan Gupta <srajang544@gmail.com>
2026-07-05 12:34:45 +0530

    Update TownScene.js

--- src/scenes/TownScene.js
+++ src/scenes/TownScene.js
@@ -8,6 +8,7 @@ import { isLightboxOpen, closeLightbox } from '../ui/Lightbox.js';
 import { AudioManager } from '../audio/AudioManager.js';
 import { registerUiSound } from '../ui/uiSound.js';
 import { loadSave, saveState } from '../save/SaveManager.js';
+import { isRatingOpen, closeRating } from '../ui/RatingPanel.js';
 
 const hintEl = document.getElementById('interact-hint');
 const hintLabelEl = document.getElementById('interact-hint-label');
@@ -626,11 +627,12 @@ export class TownScene extends Phaser.Scene {
     this.interactKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
     this.interactKey.on('down', () => this._tryInteract());
 
-    this.input.keyboard.on('keydown-ESC', () => {
-      if (isLightboxOpen()) closeLightbox();
-      else if (isOpen()) closeSection();
-      else if (isDialogueOpen()) closeDialogue();
-    });
+   this.input.keyboard.on('keydown-ESC', () => {
+  if (isLightboxOpen()) closeLightbox();
+  else if (isRatingOpen()) closeRating();   // ← yeh naya add kiya
+  else if (isOpen()) closeSection();
+  else if (isDialogueOpen()) closeDialogue();
+});
   }
 
   _setupTouchControls() {
